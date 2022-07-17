267d615e491a0991d307c35533bdecac16640968
Romilson Luis <romilsonlonan@gmail.com>
2022-07-17 09:07:24 -0300

    Twilio SMS

--- backend/src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
+++ backend/src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
@@ -1,5 +1,7 @@
 package com.devsuperior.dsmeta.controllers;
-
+/* O "COMTROLLER" TEM A FUNÇÃO DE IMPLEMENTAR UMA API.
+É O CONTROLLER QUE VAI DISPONIBILIZAR
+OS ENDPOINTS QUE O FRONTEND VAI PRECISAR PARA ACESSAR O BACKEND */
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,22 +21,22 @@ public class SaleController {
 /*PADRÃO DE 3 CAMADAS(CONTROLLER CHAMA O SERVICE,
 SERVICE CHAMA O CONTROLLER */
 
-@Autowired
-private SaleService service;
+	@Autowired
+	private SaleService service;
+	
+	@Autowired
+	private SmsService smsService;
+	
+	@GetMapping
+	public Page<Sale> findSales(
+	@RequestParam(value="minDate", defaultValue = "") String minDate,
+	@RequestParam(value="maxDate", defaultValue = "") String maxDate,
+	Pageable pageable) {
+	return service.findSales(minDate, maxDate, pageable);
+	}
 
-@Autowired
-private SmsService smsService;
-
-@GetMapping
-public Page<Sale> findSales(
-@RequestParam(value="minDate", defaultValue = "") String minDate,
-@RequestParam(value="maxDate", defaultValue = "") String maxDate,
-Pageable pageable) {
-return service.findSales(minDate, maxDate, pageable);
-}
-
-@GetMapping("/{id}/notification")
-public void notifySms(@PathVariable Long id) {
-smsService.sendSms(id);
-}
+	@GetMapping("/{id}/notification")
+	public void notifySms(@PathVariable Long id) {
+	smsService.sendSms(id);
+	}
 }
