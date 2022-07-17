67ceca164c59713dcfe9d7e0ad804608bc60c0fc
Romilson Luis <romilsonlonan@gmail.com>
2022-07-17 13:17:33 -0300

    Toast

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
-import Header from "./components/Header"
-import SalesCard from "./components/SalesCard"
+import Header from "./components/Header";
+import SalesCard from "./components/SalesCard";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
